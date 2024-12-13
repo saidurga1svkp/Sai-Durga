0bc5c60b7486ea402d4e90e031778590d6176e92
LPT-28 <LPT-28@192.168.1.62>
2024-12-13 16:32:43 +0530

    First Commit

---
+++ svkp_college/src/Collections_Demo/TreeSetDemo.java
@@ -0,0 +1,23 @@
+package Collections_Demo;
+
+
+	import java.util.LinkedHashSet;
+	import java.util.Set;
+	import java.util.TreeSet;
+
+	public class TreeSetDemo {
+
+		public static void main(String[] args) {
+			Set<String> set=new TreeSet<>();
+			set.add("cherry");
+			set.add("banana");
+			set.add("apple");
+			
+			set.add("cherry");
+			
+			System.out.println("Treeset " +set);
+		}
+
+	}
+
+
