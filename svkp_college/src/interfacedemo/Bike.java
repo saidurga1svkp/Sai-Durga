2de7ee8097e1260fcf97c12e2b63cff5139c999f
LPT-28 <LPT-28@192.168.1.62>
2024-12-16 15:10:16 +0530

    First Commit

---
+++ svkp_college/src/interfacedemo/Bike.java
@@ -0,0 +1,27 @@
+package interfacedemo;
+
+
+	//another class implementingn the same interface
+	public class Bike implements Vehicle
+	{
+	     private String type;
+	     
+	     public Bike(String type)
+	     {
+	    	 this.type=type;
+	     }
+		@Override
+		public void start() {
+			
+			System.out.println("Bike " + type + " is starting");
+		}
+
+		@Override
+		public void stop() {
+			
+			System.out.println("Bike " + type+ " is stopping");
+		}
+
+	}
+
+
