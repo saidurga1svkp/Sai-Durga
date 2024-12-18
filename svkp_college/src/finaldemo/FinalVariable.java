885b6b3991b61eaa27872c3c32f0507e2c629340
LPT-28 <LPT-28@192.168.1.62>
2024-12-18 15:07:15 +0530

    First Commit

---
+++ svkp_college/src/finaldemo/FinalVariable.java
@@ -0,0 +1,13 @@
+package finaldemo;
+
+
+	public class FinalVariable {
+
+		public static void main(String[] args) {
+			int num=8;
+			System.out.println(num);
+			
+		}
+
+	}
+
---
+++ svkp_college/src/finaldemo/Finalclass.java
@@ -0,0 +1,29 @@
+package finaldemo;
+
+
+	 class calc //hema
+	{
+		public void show()
+		{
+			System.out.println("AI integration done by hema");
+		}
+	}
+
+	class adav extends  calc //yasar
+	{
+		public void show()
+		{
+			System.out.println("AI integration done by yasar");
+		}
+	}
+
+
+
+	public class Finalclass {
+
+		public static void main(String[] args) {
+			
+
+		}
+
+	}
